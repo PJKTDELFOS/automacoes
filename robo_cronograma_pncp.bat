425810f8983681f9caf876704ef8113211c445ef
PJKTDELFOS <albert.franca1992@gmail.com>
2025-12-29 21:54:56 -0300

    Primeiro commit

---
+++ robo_cronograma_pncp.bat
@@ -0,0 +1,10 @@
+@echo off
+title Rodando Robo de Licitacoes PNCP
+echo Verificando licitacoes e enviando e-mail...
+
+:: Usando o caminho completo do Python que apareceu no seu log
+"C:\Users\Pichau\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\Pichau\Desktop\PROJETOS\bot_pncp\cronograma_via_api.py"
+
+echo.
+echo Processo concluido!
+pause
