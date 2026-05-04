28246476e8a43926b644b61b5e4c6f3a9b58342f
PJKTDELFOS <albert.franca1992@gmail.com>
2026-05-04 20:47:41 -0300

    subir container docker para vps

--- engine_busca_pncp/coletor_central.py
+++ engine_busca_pncp/coletor_central.py
@@ -56,7 +56,7 @@ class ColetorCentral:
                 self.db.registrar_mapeamento_diario_PNCP(data_referencia,total_paginas)
             else:
                 print(f"[-] Falha ao acessar API para mapeamento: {resp.status_code}, ")
-                return 0
+                return False
 
             while True:
                 tarefa=self.db.get_proxima_pagina_PNCP(data_referencia)
@@ -111,7 +111,7 @@ class ColetorCentral:
             self.log.registro('SISTEMA','COLETOR',
                               'CRITICAL','COL_FAIL','ERRO CATASTROFICO',str(e))
             print(f"[-] Erro crítico: {e}, 'caindo aqui" )
-            return 0
+            return False
 
 
 
